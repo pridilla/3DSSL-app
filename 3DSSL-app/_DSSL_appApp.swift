1e1000b2ff8d5f08f2bd7441b1f2afe1e834ca9c
Peter Ridilla <peter.ridilla@tum.de>
2024-05-05 09:00:47 +0200

    Initial Commit

---
+++ 3DSSL-app/_DSSL_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  _DSSL_appApp.swift
+//  3DSSL-app
+//
+//  Created by Peter Ridilla on 05.05.24.
+//
+
+import SwiftUI
+
+@main
+struct _DSSL_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
